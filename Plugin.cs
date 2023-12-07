d5f8d4cb2847b971c24df9d10254aac43423d69c
P0pMan20 <51844807+P0pMan20@users.noreply.github.com>
2023-12-07 16:50:34 +1000

    creating a github

---
+++ Plugin.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using BepInEx;
+using HarmonyLib;
+using UnityEngine;
+using UnityEngine.Analytics;
+
+namespace FloopyLessRogueLikeMoreRogueLite
+{
+    [BepInPlugin("pop.mods.creditrollover", "Credit Rollover", "1.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+            Harmony.CreateAndPatchAll(typeof(PatchCreditsToRollOver));
+        }
+    }
+}
+
+class PatchCreditsToRollOver
+{
+
+
+    static private int creds = 0;
+    [HarmonyPatch(typeof(GameNetworkManager), "ResetSavedGameValues")]
+    [HarmonyPrefix]   
+    static void GrabCreditAmountBeforeWipe(ref GameNetworkManager __instance)
+    {
+        
+        // __state = ES3.Load<int>("GroupCredits", __instance.currentSaveFileName);
+        creds = GameObject.FindObjectOfType<Terminal>().groupCredits;
+        UnityEngine.Debug.Log($"We have {creds} creds");
+
+    }
+    
+    [HarmonyPatch(typeof(GameNetworkManager), "ResetSavedGameValues")]
+    [HarmonyPostfix]
+    static void SetCredit(ref GameNetworkManager __instance)
+    {
+        UnityEngine.Debug.Log($"We have {creds} creds");
+        ES3.Save<int>("GroupCredits", creds, __instance.currentSaveFileName);
+    }
+    [HarmonyPatch(typeof(StartOfRound), "ResetShip")]
+    [HarmonyPostfix]
+    static void SetCreditTerminal(ref StartOfRound __instance)
+    {
+        UnityEngine.Debug.Log($"We have {creds}");
+        GameObject.FindObjectOfType<Terminal>().groupCredits = creds;
+    }
+
+
+}
---
+++ README.md
@@ -0,0 +1,18 @@
+# Credits Rollover
+
+![](https://cdn.discordapp.com/attachments/926749935263698967/1182209177641300069/banner.png?ex=6583dd18&is=65716818&hm=253f7bcda6c51750d3176b275e79621a464308b8acc74acb3f1c18d92bef48e9&)
+
+This mod makes your credits rollover to the next round after you get ejected.
+
+# Incompatibilities
+
+Potentially anything that messes with Terminal.groupCredits or anything that pre/postfixes ResetSavedGameValues or ResetShip.
+
+### Issues
+
+[GitHub](https://github.com/P0pMan20/CreditRollover) or contact @P0pMan20 on the lethal company community discord
+
+### Todo
+
+-[ ] Put code on Github.
+-[ ] Allow user to specfic rollover percentage in config.
